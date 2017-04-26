7bee65e92d5c8f6a27b19f602974fc32ce652424
-KaiK- <enriqueoriol@gmail.com>
2017-04-26 14:11:39 +0200

    3 - Progress bar II: Input decorator and input bindings

--- src/app/progress-bar/progress-bar.component.ts
+++ src/app/progress-bar/progress-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-progress-bar',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 export class ProgressBarComponent {
 
+  @Input() progress:number = 0;
+  
   constructor() { }
 
 }
