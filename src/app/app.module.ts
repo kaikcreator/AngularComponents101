454d37d01402f8c17f6dd33d8a07a5af6bb2053e
-KaiK- <enriqueoriol@gmail.com>
2017-04-26 16:13:14 +0200

    4 - Countdown component: OnInit lifecycle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProgressBarComponent } from "./progress-bar/progress-bar.component";
+import { CountdownComponent } from "./countdown/countdown.component";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProgressBarComponent
+    ProgressBarComponent,
+    CountdownComponent
   ],
   imports: [
     BrowserModule,
--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit {
+    ngOnInit(): void {
+      this.startCountdown();
+    }
+
+
+  @Input() init:number = null;
+  public counter:number = 0;
 
   constructor() { }
+
+  startCountdown(){
+    if(this.init && this.init >0){
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown(){
+    setTimeout(()=>{
+      this.counter = this.counter -1;
+      this.processCountdown();
+    }, 1000);
+  }
+
+  processCountdown(){
+    //emit event COUNT
+    console.log("count is ", this.counter);
+
+    if(this.counter == 0){
+      //emit event COUNTER END
+      console.log("--counter end--");
+    }
+    else{
+      this.doCountdown();
+    }
+  }
 
 }
